0254105d7878f2de0627e47e1269d883ac033bdc
davidmgudeman <davidmgudeman@gmail.com>
2017-08-09 15:25:49 -0700

    Initial Commit

---
+++ StackExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackExample
+//
+//  Created by David Gudeman on 8/9/17.
+//  Copyright © 2017 dmgudeman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
